a8cc5477ea5f9aa817438a6661d6b894b2f7a72b
Tunde Ajao <tunde_ajao@hotmail.com>
2022-09-03 07:14:46 +0100

    Added additional Stuff

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.set('view engine', 'ejs');
 
 app.listen(3000);
 
+app.use((req, res)=> {
+    console.log('New Request Made');
+    console.log('Host: ', req.hostname);
+    console.log('Path: ', req.path);
+    console.log('Method: ', req.method);
+});
+
+//index/home
 app.get('/', (req, res) => {
     const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
@@ -26,6 +34,7 @@ app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Create A New Blog'});
 });
 
+//404 Request
 app.use((req, res)=> {
     //res.status(404).sendFile('./views/404.html', {root:__dirname});
     res.status(404).render('404', {title: '404'});
