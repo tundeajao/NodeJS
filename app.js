b7f8e0d0ee1fa6173c02d26470b673ff9f920c62
Tunde Ajao <tunde_ajao@hotmail.com>
2022-09-03 07:47:45 +0100

    Adding Morgan middle ware

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 app.set('view engine', 'ejs');
 //app.set('views', 'myviews');
 
 app.listen(3000);
 
-app.use((req, res)=> {
-    console.log('New Request Made');
-    console.log('Host: ', req.hostname);
-    console.log('Path: ', req.path);
-    console.log('Method: ', req.method);
-});
+app.use(morgan('tiny'));
 
 //index/home
 app.get('/', (req, res) => {
