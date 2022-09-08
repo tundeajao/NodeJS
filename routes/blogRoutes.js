c5765f70da1029225c9e9e6dba45387ee88d69a8
Dr Tunde Ajao <tunde_ajao@hotmail.com>
2022-09-09 00:59:42 +0100

    Additional Formating

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,8 +3,6 @@ const Blog = require('../models/blog');
 const router = express.Router();
 const blogController = require('../controllers/blogController');
 
-
-
 router.get('/', blogController.blog_index);
 router.post('/', blogController.blog_create_post);
 router.get('/create', blogController.blog_create_get);
